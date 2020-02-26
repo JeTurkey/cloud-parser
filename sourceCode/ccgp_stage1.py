b31f013870387f3e557c04808651cf7db2c83a95
Ray Shi <rayshi@dyn-118-138-12-117.its.monash.edu.au>
2020-02-26 16:09:23 +1100

    minor update

--- sourceCode/ccgp_stage1.py
+++ sourceCode/ccgp_stage1.py
@@ -138,7 +138,7 @@ while int(page_index) <= pageCount:
     # page_index 递增
     page_index += 1
     # 最终页终止条件
-    if page_index >= pageCount:
+    if page_index > pageCount:
         print('All finished')
         break
     # 读取新的一页
