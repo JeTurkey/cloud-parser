2736b275aa154e51e2980d31bbfa31af7019fc32
Rui Shi <ray.ruishi@hotmail.com>
2020-08-12 16:24:17 +0800

    update

--- sourceCode/mofcom.py
+++ sourceCode/mofcom.py
@@ -98,7 +98,7 @@ def main():
                 if 'http' in item.find('a').get('href'):
                     results.append(item.find('a').get('href'))
                 else:
-                    results.append('http://www.mofcom.gov.cn' + tmp.find('a').get('href'))
+                    results.append('http://www.mofcom.gov.cn' + item.find('a').get('href'))
 
         # # 要闻
 
