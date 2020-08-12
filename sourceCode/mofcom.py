bf8c037f58d59ff7f03dcce51b37d0d05dd4bb67
Rui Shi <ray.ruishi@hotmail.com>
2020-08-12 16:21:43 +0800

    mofcom added

---
+++ sourceCode/mofcom.py
@@ -0,0 +1,145 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+import pymongo
+import random, time
+
+def output(str, date, sysEnv):
+    if sysEnv is 1 or sysEnv is "1":
+        openfile = open('/Users/rayshi/Desktop/cloud-parser/news/peopleFinance' + date + '.txt', 'a+')
+        print(str)
+        openfile.write(str)
+        openfile.close()
+    elif sysEnv is 2 or sysEnv is "2":
+        openfile = open('/home/admin/cloud-parser/news/peopleFinance' + date + '.txt', 'a+')
+        print(str)
+        openfile.write(str)
+        openfile.close()
+    else:
+        print('You fuck head, you have entered a invalid system, and you caused the system crash')
+        sys.exit()
+
+
+def randomePause():
+
+    # randomTime = random.randint(6000, 1200)
+    randomTime = 6000
+        
+    print('About to rest ', randomTime, ' s')
+    time.sleep(randomTime)
+
+def minorRandomPause():
+    randomTime = random.randint(300, 600)
+    print('About to rest ', randomTime, ' s')
+    time.sleep(randomTime)
+
+def majorRandomPause():
+    randomTime = random.randint(1800, 3600)
+    print('About to enter major sleep ', randomTime, ' s')
+    time.sleep(randomTime)
+
+# ===================  这边每个版本都需要改  Start ==========================
+
+def parsingContent(link):
+    page = requests.get(link)
+    s = BeautifulSoup(page.content, features="html.parser")
+    print('getting ', link)
+
+    title = ''
+
+    content = ''
+
+    try:
+        title = s.find('h1', {'id': 'artitle'}).text.strip()
+    except:
+        print('title extraction error')
+
+    try:
+        contentList = s.find('div', {'id': 'zoom', 'class': 'artCon'}).findAll('p')
+        for p in contentList:
+            content += str(p)
+    except:
+        print('content Extraction error')
+
+    timeFormat = str(time.localtime().tm_year) + '-' + str(time.localtime().tm_mon) + '-' + str(time.localtime().tm_mday) + '-' + str(time.localtime().tm_hour)
+
+    rst = {'urlLink': link, 'title': title, 'source': '光明网财经', 'content': content, 'dateAdded': timeFormat}
+    
+    return rst
+
+# ================== 这边每个版本都需要改  End ===========================
+
+    
+
+def main():
+    print('Program initiating ... ...')
+    status = True
+
+    # making mongo connection
+    myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+    mydb = myclient['ttd']
+    mycol = mydb['news']
+
+    
+
+    while status:
+        r = requests.get('http://www.mofcom.gov.cn/article/ae/') # URL 地址
+        soup = BeautifulSoup(r.content, features="html.parser")
+        print('欢迎来到中华人民共和国商务部新闻页面')
+        results = [] # 储存结果
+        # 获取这一页所有title 
+        
+        # 头版靠左
+        links = soup.find('section', {'class': 'w1200 m-con-01'}).findAll('section')[:-2]
+
+        for link in links:
+            tmp = link.findAll('li')
+            for item in tmp:
+                if 'http' in item.find('a').get('href'):
+                    results.append(item.find('a').get('href'))
+                else:
+                    results.append('http://www.mofcom.gov.cn' + tmp.find('a').get('href'))
+
+        # # 要闻
+
+        # yaowen = soup.find('div', {'class': 'part2 inner1000 clearfix'}).find('ul', {'class': 'arc_list_t1 clearfix'}).findAll('li')
+        # for link in yaowen:
+        #     if 'http' not in link.find('a').get('href'):
+        #         results.append('https://economy.gmw.cn/' + link.find('a').get('href'))
+        #     else:
+        #         results.append(link.find('a').get('href'))
+
+        # # 产经板块
+
+        # chanjing = soup.find('ul', {'class': 'arc_pic_list'}).findAll('li')
+        # for link in chanjing:
+        #     if 'http' not in link.find('a').get('href'):
+        #         results.append('https://economy.gmw.cn/' + link.find('a').get('href'))
+        #     else:
+        #         results.append(link.find('a').get('href'))
+
+
+        print('This round the result has ', len(results), ' items')
+
+        new_results = []
+
+        for key in results:
+            if mycol.count_documents({"urlLink": key}) > 0:
+                print(key, ' key existed')
+            else:
+                new_results.append(key)
+                print(key, ' New Key Added')
+
+        if len(new_results) == 0:
+            majorRandomPause()
+        else:
+            for key in new_results:
+                mycol.insert_one(parsingContent(key))
+                minorRandomPause()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
