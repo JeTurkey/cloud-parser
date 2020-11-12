2050f578be7089e4e1a51046dddd13602bb7958f
Ray Shi <rayshi@nothing.attdns.com>
2020-11-12 19:08:32 +0800

    mofcom_new update

--- sourceCode/mofcom_new.py
+++ sourceCode/mofcom_new.py
@@ -18,12 +18,12 @@ def minorRandomPause():
     time.sleep(randomTime)
 
 def connectDB():
-    # mydb = mysql.connector.connect(host='rm-bp11g1acc24v9f69t1o.mysql.rds.aliyuncs.com',
-    #                             user='rayshi',
-    #                             password='Rayshi1994!',
-    #                             database='ttd',
-    #                             auth_plugin='mysql_native_password')
-    mydb = mysql.connector.connect(host='localhost', user='root', password='Rayshi1994!', database='ttd')
+    mydb = mysql.connector.connect(host='rm-bp11g1acc24v9f69t1o.mysql.rds.aliyuncs.com',
+                                user='rayshi',
+                                password='Rayshi1994!',
+                                database='ttd',
+                                auth_plugin='mysql_native_password')
+    # mydb = mysql.connector.connect(host='localhost', user='root', password='Rayshi1994!', database='ttd')
 
     
     print('DB is connected')
