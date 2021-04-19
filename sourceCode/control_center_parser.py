b72ac350edf8bbf4b90e2e7c9eed04348e2f0c06
Ray Shi <rayshi_1994@hotmail.com>
2021-04-19 21:08:01 +0800

    remove eastfund

--- sourceCode/control_center_parser.py
+++ sourceCode/control_center_parser.py
@@ -1,12 +1,12 @@
 import schedule
 import time
-import parser_eastFund
+# import parser_eastFund
 import parser_caixin
 import parser_chinaSecurity
 import parser_chinaPeace
 
 # ============== 爬虫任务管理 =================
-schedule.every(1.5).hours.do(parser_eastFund.main) # 东方财富基金资讯
+# schedule.every(1.5).hours.do(parser_eastFund.main) # 东方财富基金资讯
 schedule.every().hour.do(parser_caixin.main) # 财新网
 schedule.every().hour.do(parser_chinaSecurity.main) # 中国证券报
 schedule.every().day.at('23:10').do(parser_chinaPeace.main) # 中央政法委
