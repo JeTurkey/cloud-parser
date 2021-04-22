3c857e324524052a35b8d338539f85c8a71bc82f
Ray Shi <rayshi_1994@hotmail.com>
2021-04-22 22:10:07 +0800

    minor fix on gwyzc

--- sourceCode/control_center_parser.py
+++ sourceCode/control_center_parser.py
@@ -4,12 +4,14 @@ import time
 import parser_caixin
 import parser_chinaSecurity
 import parser_chinaPeace
+import parser_gwyzc
 
 # ============== 爬虫任务管理 =================
 # schedule.every(1.5).hours.do(parser_eastFund.main) # 东方财富基金资讯
 schedule.every().hour.do(parser_caixin.main) # 财新网
 schedule.every().hour.do(parser_chinaSecurity.main) # 中国证券报
 schedule.every().day.at('23:10').do(parser_chinaPeace.main) # 中央政法委
+schedule.every(5).hours.do(parser_gwyzc.main) # 国务院政策
 
 
 # ============== 爬虫任务管理 END =================
